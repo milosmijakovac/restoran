0c32b9b5cbc1409fd27c6d5530667ff45c9976ef
Milos <skapha@gmail.com>
2019-02-01 16:16:33 +0100

    gallery added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,19 @@ import SEO from '../components/seo'
 import { HomeHeader, Banner, BannerButton } from '../utils'
 import img from '../images/bcg/homeBcg.jpeg'
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
+import Gallery from '../components/HomePageComponents/Gallery-1'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader img={img}>
       <Banner title="eatery" subtitle="Pere Segedinca 2 - Pancevo">
-        <BannerButton style={{margin: '2rem auto'}}>menu</BannerButton>
+        <BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
       </Banner>
     </HomeHeader>
     <QuickInfo />
+    <Gallery />
   </Layout>
 )
 
